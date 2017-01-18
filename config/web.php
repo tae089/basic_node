141cf083271fd9382ee343471a7efe26e6d2a01c
tae <amager084@gmail.com>
2017-01-18 14:23:14 +0700

    first commit

---
+++ config/web.php
@@ -0,0 +1,76 @@
+<?php
+
+$params = require(__DIR__ . '/params.php');
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+      'bootstrap' => ['log'],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'uBATmq99vGS7zN63nVFOq6q8up7Fny1f',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => require(__DIR__ . '/db.php'),
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        */
+    ],
+    'modules'    => [
+        'gridview' => [
+            'class' => '\kartik\grid\Module',
+            // enter optional module parameters below - only if you need to
+            // use your own export download action or custom translation
+            // message source
+            // 'downloadAction' => 'gridview/export/download',
+            // 'i18n' => []
+        ],
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
---
+++ views/product/index.php
@@ -0,0 +1,77 @@
+<?php
+
+use yii\helpers\Html;
+use kartik\grid\GridView;
+use yii\widgets\Pjax;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\ProductSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Products';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="product-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Product', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?php Pjax::begin(['options' => ['data-pjax' => true ], 'id'=>'countries']) ?>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'showOnEmpty'=>true,
+        'panel'=>['type'=>'primary', 'heading'=>'จัดการประเภทสมาชิก'],
+        'responsive'=>true,
+        'hover'=>true,
+        //'pjax'=>true,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'product_code',
+            'product_name',
+            'product_price',
+            'created_at',
+
+            [
+            'class' => 'yii\grid\ActionColumn',
+            'buttonOptions'  => ['class' => 'btn btn-default'],
+            'header'         => '<font size=2>จัดการ</font>',
+            'headerOptions'  => ['class' => 'text-center skip-export'],
+            'contentOptions' => ['class' => 'text-center skip-export', 'noWrap' => true],
+            'template'       => '{update} {delete1}',
+            'buttons'        => [
+                    'delete1' => function($model,$index){
+                        $btn_del =  Html::a('<span class="glyphicon glyphicon-trash"></span>', $url = null, ['title'=>'Delete', 'data-pjax'=>'0','id'=>"btn_del'.$index.'",'rel'=>'/index.php?r=product/delete','class'=>'btn btn-danger']);
+                        return $btn_del;
+                    },
+                ],
+            ],
+        ],
+    ]); ?>
+    <?php Pjax::end() ?>
+</div>
+<!--<ul class="list-group"></ul>-->
+<?php
+$this->registerJsFile('@web/js/socket.io/dist/socket.io.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
+$head = '$("document").ready(function(){';
+$head.='
+  var socket = io.connect("http://localhost:8080");
+
+  socket.on("addOK",function(data){
+     $.pjax.reload({container:"#countries"});  //Reload GridView
+  });
+
+//Delete
+  $("#btn_del").click(function(){
+    alert(123);
+  });
+
+';
+$head.='});';
+$this->registerJs($head);
+?>
